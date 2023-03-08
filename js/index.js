9c7c97f38b7617a72cfc7bfc99f505380d0ad772
mustafo-07 <odilovm291.gmail.com>
2023-03-08 15:05:48 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// const year = [1985, 1995, 2000, 2004, 1950]
+
+// function myFunction(number) {
+//     return number - 2023;
+// }
+// for (let i = 0; i < year.length; i++) {
+//     console.log(myFunction(year[i]));
+
+// }
+
+
+
+// ikinchi uy ishi
+
+
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function myFunction(number) {
+    return number * 2;
+}
+for (let i = 0; i < num.length; i++) {
+    console.log(myFunction(num[i]));
+
+}
